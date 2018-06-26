b77a32dfe66c25d5e3d8103a3038401063c302cd
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-26 21:27:15 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,7 @@ class School
 
 
   def add_student(name, grade)
-    roster[grade] = [] unless roster.values.include?(grade)
+    roster[grade] = [] if roster.values.include?(grade)
     roster[grade] << name
   end
 
